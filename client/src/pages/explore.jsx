5d331180560f8928ac4282ac8246133c4feae93f
Joel Mathew Job <joelmathewjob36@gmail.com>
2024-07-04 19:03:53 +0530

    updated th explore section width

--- client/src/pages/explore.jsx
+++ client/src/pages/explore.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function explore() {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center '>
 
-      <div className="flex flex-wrap p-5 -m-1 md:w-2/3 md:-m-2">
+      <div className="flex flex-wrap p-5 -m-1 lg :w-2/3 md:-m-2">
         <div className="flex flex-wrap w-1/2">
           <div className="w-1/2 p-1 md:p-2">
             <img alt="gallery" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/500x300" />
